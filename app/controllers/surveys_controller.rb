7097bfd64af3c5b5a7fa6ec3edaa9fa5057e0dac
Adrien Bigler <adrien.bigler@heig-vd.ch>
2016-08-23 13:59:24 +0200

    Setup slim

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -2,6 +2,14 @@ class SurveysController < ApplicationController
   def new
   end
   def create
-  	render plain: params[:survey].inspect
+  	# render plain: params[:survey].inspect
+  	@survey = Survey.new(survey_params)
+ 
+  	@survey.save
+  	redirect_to @survey
 	end
 end
+private
+  def survey_params
+    params.require(:survey).permit(:q1, :q2)
+  end
