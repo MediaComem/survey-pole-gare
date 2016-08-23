f840a918bc01a531f4a916f4ec35b69cd7d877a8
Adrien Bigler <adrien.bigler@heig-vd.ch>
2016-08-23 10:52:12 +0200

    Setup pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160823082132) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "surveys", force: :cascade do |t|
     t.string   "q1"
     t.text     "q2"
