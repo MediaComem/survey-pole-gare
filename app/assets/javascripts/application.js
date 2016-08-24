e9740a046935a53afe42ecf77392200eafed232b
Adrien Bigler <adrien.bigler@heig-vd.ch>
2016-08-24 14:26:10 +0200

    Ol setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require ol
 //= require_tree .
---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+	console.log( "ready!" );
+	var map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([37.41, 8.82]),
+      zoom: 4
+    })
+  });
+});
