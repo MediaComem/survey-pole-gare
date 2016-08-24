694b1a7fc26fd1a5d775c7d0e4d7806a0b39e64e
Adrien Bigler <adrien.bigler@heig-vd.ch>
2016-08-24 09:31:10 +0200

    Test form
    Fields for is ok

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -2,14 +2,14 @@ class SurveysController < ApplicationController
   def new
   end
   def create
-  	# render plain: params[:survey].inspect
-  	@survey = Survey.new(survey_params)
+  	render plain: params[:survey].inspect
+  	# @survey = Survey.new(survey_params)
  
-  	@survey.save
-  	redirect_to @survey
+  	# @survey.save
+  	# redirect_to @survey
 	end
 end
 private
   def survey_params
-    params.require(:survey).permit(:q1, :q2)
+    params.require(:survey).permit(:q1, :q2, :q3, :q4)
   end
