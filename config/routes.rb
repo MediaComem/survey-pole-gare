6dcd3b6970dc0c570981463d74bd4aa4158779f0
Adrien Bigler <adrien.bigler@heig-vd.ch>
2016-09-08 15:15:42 +0200

    Change base route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 	get '/surveys/admin(.:format)', to: 'surveys#admin', as: 'survey_admin'
+
+	get '/', to: 'surveys#new'
   resources :surveys
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
